8124c3f40f87e2d87dfaeef0e325c72691a7f1af
Dariusz Konopka <drkknpk@gmail.com>
2017-02-11 14:34:43 +0100

    RocketStart implemented

--- SomeProgram.java
+++ SomeProgram.java
@@ -4,7 +4,7 @@ public class SomeProgram {
 	System.out.println("We will be Git experts, soon");
     }
 	
-	public static void StartRocket(){
-		//to do some code here!
-	}
+    public static void StartRocket(){
+        //to do some code here!
+    }
 }
